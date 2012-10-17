87f86e72a737325972c64ba655ea07295e5e172a
Alexander Kharitonov <alexander.kharitonov@gmail.com>
2012-10-17 16:01:16 +0400

    Fixed io_zconf.
    Do not delete ZK-managed memory.

--- phantom/io_zconf/io_zconf.C
+++ phantom/io_zconf/io_zconf.C
@@ -1,33 +1,18 @@
-//TODO(skywalker): prepend path to keys, handle session reset
-// 
 #include <phantom/io_zconf/io_zconf.H>
+#include <phantom/io_zhandle/zoo_util.H>
 
 #include <pd/base/exception.H>
 #include <pd/base/log.H>
 
 #include <phantom/module.H>
 
-#include <zookeeper.h>
+#include <zookeeper/zookeeper.h>
 
 namespace phantom {
 
 MODULE(io_zconf);
 
 namespace {
-
-template<typename T>
-class free_guard_t {
-public:
-    free_guard_t(T* ptr)
-        : ptr_(ptr)
-    {}
-
-    ~free_guard_t() {
-        free((void*)ptr_);
-    }
-private:
-    T* ptr_;
-};
 
 template<typename T>
 class delete_guard_t {
@@ -42,53 +27,6 @@ public:
 private:
     T* ptr_;
 };
-
-class Stat_guard_t {
-public:
-    Stat_guard_t(const struct Stat* stat)
-        : stat_(stat)
-    {}
-
-    ~Stat_guard_t() {
-        deallocate_Stat((Stat*) stat_);
-    }
-private:
-    const struct Stat* stat_;
-};
-
-const char* state_string(int state) {
-    if(state == ZOO_EXPIRED_SESSION_STATE) {
-            return "ZOO_EXPIRED_SESSION_STATE";
-    } else if(state == ZOO_AUTH_FAILED_STATE) {
-            return "ZOO_AUTH_FAILED_STATE";
-    } else if(state == ZOO_CONNECTING_STATE) {
-            return "ZOO_CONNECTING_STATE";
-    } else if(state == ZOO_ASSOCIATING_STATE) {
-            return "ZOO_ASSOCIATING_STATE";
-    } else if(state == ZOO_CONNECTED_STATE) {
-            return "ZOO_CONNECTED_STATE";
-    } else {
-            return "ZOO_UNKNOWN_STATE";
-    }
-}
-
-const char* event_string(int type) {
-    if(type == ZOO_CREATED_EVENT) {
-            return "ZOO_CREATED_EVENT";
-    } else if(type == ZOO_DELETED_EVENT) {
-            return "ZOO_DELETED_EVENT";
-    } else if(type == ZOO_CHANGED_EVENT) {
-            return "ZOO_CHANGED_EVENT";
-    } else if(type == ZOO_CHILD_EVENT) {
-            return "ZOO_CHILD_EVENT";
-    } else if(type == ZOO_SESSION_EVENT) {
-            return "ZOO_SESSION_EVENT";
-    } else if(type == ZOO_NOTWATCHING_EVENT) {
-            return "ZOO_NOTWATCHING_EVENT";
-    } else {
-            return "ZOO_UNKNOWN_EVENT";
-    }
-}
 
 }  // anonymous namespace
 
@@ -166,7 +104,7 @@ public:
         } else if(rc == ZINVALIDSTATE) {
             new get_item_t(&zconf_, key_);
         } else {
-            log_error("zoo_awget returned %d (%m)", rc);
+            log_error("zoo_awget returned %d (%s)", rc, zerror(rc));
             fatal("unknown state in get_item_t");
         }
     }
@@ -211,7 +149,7 @@ public:
         } else if(rc == ZINVALIDSTATE) {
             new watch_item_t(&zconf_, key_);
         } else {
-            log_error("zoo_awexists returned %d (%m)", rc);
+            log_error("zoo_awexists returned %d (%s)", rc, zerror(rc));
             fatal("unknown state in watch_item_t");
         }
     }
@@ -241,7 +179,8 @@ public:
         if(var_map.find(set_data_.key) == var_map.end()) {
             log_debug("key '%s' removed, not setting", key_z);
             bq_cond_guard_t set_guard(set_data_.cond);
-            set_data_.cond.send(); // XXX ignoring nonwatched sets
+            set_data_.success = false;
+            set_data_.cond.send();
             return;
         }
         guard.relax();
@@ -264,7 +203,7 @@ public:
         } else if(rc == ZINVALIDSTATE) {
             new set_item_t(&zconf_, set_data_);
         } else {
-            log_error("zoo_aset returned %d (%m)", rc);
+            log_error("zoo_aset returned %d (%s)", rc, zerror(rc));
             fatal("unknown state in zoo_aset");
         }
     }
@@ -353,10 +292,9 @@ void io_zconf_t::node_watcher(zhandle_t*,
 {
     io_zconf_t* iozc = reinterpret_cast<io_zconf_t*>(ctx);
 
-    free_guard_t<const char> path_guard(path);
     log_debug("node watch: state %s, event %s at '%s'",
-             state_string(state),
-             event_string(type),
+             zoo_util::state_string(state),
+             zoo_util::event_string(type),
              path);
     size_t path_len = strlen(path);
     string_t key = string_t::ctor_t(path_len)(str_t(path, path_len));
@@ -371,7 +309,7 @@ void io_zconf_t::node_watcher(zhandle_t*,
     } else if(type == ZOO_CHANGED_EVENT) {
         new get_item_t(iozc, key);
     } else {
-        log_error("Unknown event %d at node_watcher", type);
+        log_error("Unknown event %d (%s) at node_watcher", type, zoo_util::event_string(type));
         fatal("unknown state in node_watcher");
     }
 }
@@ -384,8 +322,6 @@ void io_zconf_t::data_callback(int rc,
 {
     callback_data_t* callback_data = (callback_data_t*) data;
     delete_guard_t<callback_data_t> cbdata_guard(callback_data);
-    free_guard_t<const char> value_guard(value);
-    Stat_guard_t stat_guard(stat);
 
     io_zconf_t* iozc = callback_data->iozc;
     const string_t& path = callback_data->path;
@@ -394,8 +330,10 @@ void io_zconf_t::data_callback(int rc,
         iozc->update_node(path, value, vallen, stat);
     } else if(rc == ZOPERATIONTIMEOUT) {
         new get_item_t(iozc, path);
+    } else if(rc == ZCLOSING) {
+        log_debug("data_callback: ZK closing");
     } else {
-        log_error("data_callback got rc %d", rc);
+        log_error("data_callback got rc %d (%s)", rc, zerror(rc));
         fatal("unknown state in data_callback");
     }
 }
@@ -405,8 +343,6 @@ void io_zconf_t::set_callback(int rc,
                               const void* data)
 {
     set_data_t* set_data = (set_data_t*) data;
-
-    Stat_guard_t stat_guard(stat);
 
     if(rc == ZOK) {
         bq_cond_guard_t set_guard(set_data->cond);
@@ -422,19 +358,20 @@ void io_zconf_t::set_callback(int rc,
         set_data->cond.send();
     } else if(rc == ZOPERATIONTIMEOUT) {
         new set_item_t(set_data->iozc, *set_data);
+    } else if(rc == ZCLOSING) {
+        log_debug("set_callback: ZK closing");
     } else {
-        log_error("set_callback got rc %d", rc);
+        log_error("set_callback got rc %d (%s)", rc, zerror(rc));
         fatal("unknown state in set_callback");
     }
 }
 
 void io_zconf_t::stat_callback(int rc,
-                               const struct Stat* stat,
+                               const struct Stat* /*stat*/,
                                const void* data)
 {
     callback_data_t* callback_data = (callback_data_t*) data;
     delete_guard_t<callback_data_t> cbdata_guard(callback_data);
-    Stat_guard_t stat_guard(stat);
 
     io_zconf_t* iozc = callback_data->iozc;
     const string_t& path = callback_data->path;
@@ -445,8 +382,10 @@ void io_zconf_t::stat_callback(int rc,
         iozc->set_no_node(path);
     } else if(rc == ZOPERATIONTIMEOUT) {
         new watch_item_t(iozc, path);
+    } else if(rc == ZCLOSING) {
+        log_debug("stat_callback: ZK closing");
     } else {
-        log_error("stat_callback got rc %d", rc);
+        log_error("stat_callback got rc %d (%s)", rc, zerror(rc));
         fatal("unknown state in stat_callback");
     }
 }
@@ -476,7 +415,7 @@ void io_zconf_t::update_node(const string_t& key,
     var_guard.relax();
 
     MKCSTR(val_z, var_stat.value);
-    log_info("update_node '%s' = '%s' (czxid=%ld mzxid=%ld ctime=%ld mtime=%ld version=%d cversion=%d aversion=%d ephemeralOwner=%ld dataLength=%d numChildren=%d pzxid=%ld)", key_z, val_z, stat->czxid, stat->mzxid, stat->ctime, stat->mtime, stat->version, stat->cversion, stat->aversion, stat->ephemeralOwner, stat->dataLength, stat->numChildren, stat->pzxid);
+    log_debug("update_node '%s' = '%s' (czxid=%ld mzxid=%ld ctime=%ld mtime=%ld version=%d cversion=%d aversion=%d ephemeralOwner=%ld dataLength=%d numChildren=%d pzxid=%ld)", key_z, val_z, stat->czxid, stat->mzxid, stat->ctime, stat->mtime, stat->version, stat->cversion, stat->aversion, stat->ephemeralOwner, stat->dataLength, stat->numChildren, stat->pzxid);
 }
 
 void io_zconf_t::set_no_node(const string_t& key)
@@ -503,7 +442,7 @@ void io_zconf_t::set_no_node(const string_t& key)
 }
 
 void io_zconf_t::new_session() {
-    log_info("new session, reregistering watches");
+    log_debug("new session, reregistering watches");
     
     thr::spinlock_guard_t guard(var_map_lock_);
     for(auto i = var_map_.begin(); i != var_map_.end(); ++i) {
