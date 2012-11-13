4b80bcaac23fc2b95670c91a0b185d57d606cfd4
billyevans <billyevans@yandex-team.ru>
2012-11-13 15:26:45 +0400

    Code-style fixes

--- pd/lightning/guid.C
+++ pd/lightning/guid.C
@@ -1,3 +1,4 @@
+// vim: set tabstop=4 expandtab:
 #include "guid.H"
 
 #include <pd/base/time.H>
@@ -5,23 +6,23 @@
 namespace pd {
 
 guid_generator_t::guid_generator_t(uint32_t host_id) throw()
-		: last_musec_(0), host_id_(host_id) 
+    : last_musec_(0), host_id_(host_id)
 { }
 
 uint64_t guid_generator_t::get_guid()
-{ 
-	uint64_t musec = (timeval_current() - timeval_unix_origin) / interval_microsecond;
+{
+    uint64_t musec = (timeval_current() - timeval_unix_origin) / interval_microsecond;
 
-	{ 
-		thr::spinlock_guard_t guard(last_musec_lock_);
-		if(musec > last_musec_)
-			last_musec_ = musec;
-		else
-			musec = ++last_musec_;
-	}
+    {
+        thr::spinlock_guard_t guard(last_musec_lock_);
+        if(musec > last_musec_) {
+            last_musec_ = musec;
+        } else {
+            musec = ++last_musec_;
+        }
+    }
 
-	return (musec << 12) + host_id_;
+    return (musec << 12) + host_id_;
 }
 
 }
-	
--- phantom/io_guid/io_guid.C
+++ phantom/io_guid/io_guid.C
@@ -1,3 +1,4 @@
+// vim: set tabstop=4 expandtab:
 #include "io_guid.H"
 
 #include <phantom/module.H>
@@ -7,21 +8,24 @@ namespace phantom {
 MODULE(io_guid);
 
 io_guid_t::config_t::config_t() throw()
-	: host_id(-1)
+    : host_id(-1)
 {}
 
 void io_guid_t::config_t::check(const in_t::ptr_t& p) const {
-	io_t::config_t::check(p);
+    io_t::config_t::check(p);
 
-	if(host_id == -1) {
-		config::error(p, "host_id must be set");
-	}
+    if(host_id == -1) {
+        config::error(p, "host_id must be set");
+    }
 }
 
 io_guid_t::io_guid_t(const string_t& name, const config_t& config)
-	: io_t(name, config), guid_(config.host_id)
+    : io_t(name, config), guid_(config.host_id)
 {}
 
+guid_generator_t& io_guid_t::generator() {
+    return guid_;
+}
 
 void io_guid_t::init()
 {}
