174ec40d82c550c569b27ef2514ccecfd91cd426
Philipp Sinitsyn <ph.sinitsyn@gmail.com>
2012-11-23 18:53:58 +0400

    bq_cond_guard added for push and pop

--- pd/lightning/instance_pool.C
+++ pd/lightning/instance_pool.C
@@ -1,40 +1,52 @@
 #include "pd/lightning/instance_pool.H"
 
 namespace pd {
-
 void instance_pool_t::push_open_instance(instance_id_t iid) {
-    if (open_instances_.size() >= open_instances_limit_) {
-        open_instances_not_full_.wait(NULL);    
-    } 
+	bq_cond_guard_t guard(open_instances_cond_);
+
+    while (open_instances_.size() >= open_instances_limit_) {
+        open_instances_cond_.wait(NULL);    
+    }
+
     open_instances_.push(iid);
-    open_instances_not_empty_.send();
+    open_instances_cond_.send();
 }
 
 instance_id_t instance_pool_t::pop_open_instance() {
-    if (open_instances_.size() == 0) {
-        open_instances_not_empty_.wait(NULL);
+	bq_cond_guard_t guard(open_instances_cond_);
+
+	while (open_instances_.size() == 0) {
+        open_instances_cond_.wait(NULL);
     }
+
     instance_id_t iid = open_instances_.top();
     open_instances_.pop();
-    open_instances_not_full_.send();
+    open_instances_cond_.send();
     return iid;
 }
 
 void instance_pool_t::push_reserved_instance(instance_id_t iid) {
-    if (reserved_instances_.size() >= reserved_instances_limit_) {
-        reserved_instances_not_full_.wait(NULL);
+	bq_cond_guard_t guard(reserved_instances_cond_);
+
+	while (reserved_instances_.size() >= reserved_instances_limit_) {
+        reserved_instances_cond_.wait(NULL);
     }
+
     reserved_instances_.push(iid);
-    reserved_instances_not_empty_.send();
+    reserved_instances_cond_.send();
 }
 
 instance_id_t instance_pool_t::pop_reserved_instance() {
-    if (reserved_instances_.size() == 0) {
+	bq_cond_guard_t guard(reserved_instances_cond_);
+
+	while (reserved_instances_.size() == 0) {
         reserved_instances_not_empty_.wait(NULL);
     }
+
     instance_id_t iid = reserved_instances_.top();
     reserved_instances_.pop();
-    open_instances_not_full_.send();
+    open_instances_cond_.send();
     return iid;
 }
+
 } // nmaespace pd
