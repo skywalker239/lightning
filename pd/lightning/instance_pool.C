0705cbbd21fb148790f347fde5f2eb53bb3d3b2b
Phillip Sinitsyn <phill@bscount-dev01g.yandex.net>
2012-11-27 16:45:00 +0400

    Header added; fallback to one bq_cond

--- pd/lightning/instance_pool.C
+++ pd/lightning/instance_pool.C
@@ -1,51 +1,65 @@
+// Copyright (C) 2012, Alexander Kharitonov <alexander.kharitonov@gmail.com>
+// Copyright (C) 2012, YANDEX LLC.
+// This code may be distributed under the terms of the GNU GPL v4.
+// See ‘http://www.gnu.org/licenses/gpl.html’.
+// vim: set tabstop=4 expandtab:
+
 #include "pd/lightning/instance_pool.H"
 
 namespace pd {
-void instance_pool_t::push_open_instance(instance_id_t iid) {
-    bq_cond_guard_t guard(open_instances_cond_);
+instance_pool_t::instance_pool_t(size_t open_instances_limit,
+                    		size_t reserved_instances_limit)
+		 : open_instances_limit_(open_instances_limit),
+        	reserved_instances_limit_(reserved_instances_limit),
+        	open_instances_(),
+        	reserved_instances_() {}
 
-    while (open_instances_.size() >= open_instances_limit_) {
-        open_instances_cond_.wait(NULL);    
+void instance_pool_t::push_open_instance(instance_id_t iid) {
+    bq_cond_guard_t guard(instance_cond_);
+
+    while (open_instances_.size() >= open_instances_limit_ ||
+  	   reserved_instances_.size() >= reserved_instances_limit_) {
+        instance_cond_.wait(NULL);    
     }
 
     open_instances_.push(iid);
-    open_instances_cond_.send(true);
+    instance_cond_.send(true);
 }
 
 instance_id_t instance_pool_t::pop_open_instance() {
-    bq_cond_guard_t guard(open_instances_cond_);
+    bq_cond_guard_t guard(instance_cond_);
 
     while (open_instances_.size() == 0) {
-        open_instances_cond_.wait(NULL);
+        instance_cond_.wait(NULL);
     }
 
     instance_id_t iid = open_instances_.top();
     open_instances_.pop();
-    open_instances_cond_.send(true);
+    instance_cond_.send(true);
     return iid;
 }
 
 void instance_pool_t::push_reserved_instance(instance_id_t iid) {
-    bq_cond_guard_t guard(reserved_instances_cond_);
+    bq_cond_guard_t guard(instance_cond_);
 
     while (reserved_instances_.size() >= reserved_instances_limit_) {
-        reserved_instances_cond_.wait(NULL);
+        instance_cond_.wait(NULL);
     }
 
     reserved_instances_.push(iid);
-    reserved_instances_cond_.send(true);
+    instance_cond_.send(true);
 }
 
 instance_id_t instance_pool_t::pop_reserved_instance() {
-    bq_cond_guard_t guard(reserved_instances_cond_);
+    bq_cond_guard_t guard(instance_cond_);
 
     while (reserved_instances_.size() == 0) {
-        reserved_instances_cond_.wait(NULL);
+        instance_cond_.wait(NULL);
     }
 
     instance_id_t iid = reserved_instances_.top();
     reserved_instances_.pop();
-    reserved_instances_cond_.send(true);
+    instance_cond_.send(true);
 
     return iid;
 }
